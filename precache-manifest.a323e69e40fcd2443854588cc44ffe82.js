0a0746753220b80abb872f2408102e6486493e48
Sriram Subramanian <48162551+pingsrirams@users.noreply.github.com>
2019-11-11 11:11:55 -0800

    Add files via upload

---
+++ precache-manifest.a323e69e40fcd2443854588cc44ffe82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc695081b1dcf8ab233737467749548e",
+    "url": "/React-Recipe-App/index.html"
+  },
+  {
+    "revision": "15b9511ed2ec16ddb2c0",
+    "url": "/React-Recipe-App/static/css/main.3c171a5d.chunk.css"
+  },
+  {
+    "revision": "ec1e42bb6e08378202fb",
+    "url": "/React-Recipe-App/static/js/2.521b2485.chunk.js"
+  },
+  {
+    "revision": "15b9511ed2ec16ddb2c0",
+    "url": "/React-Recipe-App/static/js/main.f3f4899c.chunk.js"
+  },
+  {
+    "revision": "e67e7a6eda48fcecf18e",
+    "url": "/React-Recipe-App/static/js/runtime-main.bce5f89e.js"
+  }
+]);
